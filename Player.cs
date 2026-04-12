08d2733b4d83f2a3e61fad131b2b8ab0187a781d
Christian <christianarrobabol@gmail.com>
2026-04-11 22:47:20 -0300

    Alguns ajustes

---
+++ Player.cs
@@ -0,0 +1,15 @@
+namespace RPGd;
+using ConsoleHelper;
+//C: A classe player é onde o jogador terá todos os seus atributos usados para interagir com o mundo
+//declarados.
+
+public class Player
+{
+    public List<string> Inventário = new List<string>();
+
+    //C: O player começa com uma banana no seu inventário.
+    public Player()
+    {
+        Inventário.Add("Banana");
+    }
+}
--- obj/Debug/net10.0/RPGd.AssemblyInfo.cs
+++ obj/Debug/net10.0/RPGd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddec3e853ed9f55896b1bbd08268709760630a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
